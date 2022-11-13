0c748935a2fa70a4eea6509b1f2cf84a7f9d33f0
renluke01 <anakarencb01@gmail.com>
2022-11-12 23:40:01 -0700

    Crear funciones calculadora2, saludos, compras

--- BasicCS/Program.cs
+++ BasicCS/Program.cs
@@ -7,8 +7,13 @@ namespace BasicCS
     {
         static void Main(string[] args) 
         {
+
             Console.WriteLine("Hello, World!");
             //Calculadora();
+            //Calculadora2();
+            //saludos();
+            compras();
+
             double a = numero1() + 5;
             double b = numero1(5) + 5;
 
@@ -50,6 +55,145 @@ namespace BasicCS
 
         }
 
+        public static void compras(){
+
+            double total;
+            int colores;
+            double descuento;
+            double tdescuento;
+
+
+            Console.WriteLine("Recibo walmart, ingresa tu total");
+            total = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Tu total es {total}, tenemos promo, elige un color para posibles descuentos: 0.Amazul 1.RosaMelo 2.Mayonesa 3.Otro");
+            colores = Int32.Parse(Console.ReadLine());
+
+
+            switch (colores)
+            {
+                case 0:
+                    Console.WriteLine ($"Elegiste Amazul, tienes un 20% de descuento.");
+                    descuento = total * 0.20; 
+                    tdescuento = total - descuento;
+                    Console.WriteLine ($"Tu descuento es de {descuento} y tu total con descuento es {tdescuento}");
+                    break;
+
+                case 1:
+                    Console.WriteLine ($"Elegiste RosaMelo, tienes un 15% de descuento.");
+                    descuento = total * 0.15; 
+                    tdescuento = total - descuento;
+                    Console.WriteLine ($"Tu descuento es de {descuento} y tu total con descuento es {tdescuento}");
+                    break;
+
+                case 2:
+                    Console.WriteLine ($"Elegiste Mayonesa, tienes un 50% de descuento.");
+                    descuento = total * 0.50; 
+                    tdescuento = total - descuento;
+                    Console.WriteLine ($"Tu descuento es de {descuento} y tu total con descuento es {tdescuento}");
+                    break;
+
+                case 3:
+                    Console.WriteLine ($"Elegiste Otro, no tienes descuento.Tas pendejo y tu total con descuento es {total}");
+                    break;
+
+                default:
+                    Console.WriteLine("no sabes leer pa");
+                    break;
+
+            }
+
+
+            
+
+        }
+
+
+        public static void saludos(){
+
+            string n;
+            string a;
+            string e;
+
+            Console.WriteLine("wenos dias, dame tu nombre");
+            n = Console.ReadLine();
+
+            Console.WriteLine("dame tu apellido");
+            a = Console.ReadLine();
+
+            Console.WriteLine("dame tu edad");
+            e = Console.ReadLine();
+        
+            int l = n.Count() / 2;
+            string mitad = n.Substring(l, 1);
+
+
+
+            Console.WriteLine ($"sabandija tu nombre es {mitad.ToUpper()}, tu apellido es {a.ToUpper().First()} y tienes {e} años");
+
+
+        }
+
+
+        public static void Calculadora2(){
+            double x;
+            double y;
+            string z;
+            double r;
+            int exit = 0;
+
+            
+
+            while (exit == 0) 
+            {
+
+                Console.WriteLine("olap soy una calculadora dame 2 numeros perro");
+                Console.WriteLine("Dame el primer numerooo");
+                x = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("dame el segundo numero");
+                y = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Elije tu operacion perro + - / *");
+                z = Console.ReadLine();
+
+
+                if(z == "+")
+                {
+                    r = x + y;
+                    Console.WriteLine($"mamaguevo tu resultado es {r}");
+
+                } 
+                else if (z == "-")
+                {
+                    r = x - y;
+                    Console.WriteLine($"mamaguevo tu resultado es {r}");
+
+                }   
+                else if (z == "/")
+                {
+                    r = x / y;
+                    Console.WriteLine($"mamaguevo tu resultado es {r}");
+
+                }
+                else if (z == "*")
+                {
+                    r = x * y;
+                    Console.WriteLine($"mamaguevo tu resultado es {r}");
+                
+                } 
+            
+                Console.WriteLine("kalkuladora pro 0.Seguir operaciones 1.salir a la verga");
+                exit = Int32.Parse(Console.ReadLine());
+
+
+            } 
+            
+
+
+
+        }
+
         private static int numero1()
         {
             return 1;
@@ -61,7 +205,7 @@ namespace BasicCS
         public static void Calculadora() 
         {
 
-            double a;
+            double j;
             double b, c;
             string opc;
 
@@ -73,7 +217,7 @@ namespace BasicCS
             {
 
                 Console.WriteLine("Escribe el primer numero verijon:");
-                a = Int32.Parse(Console.ReadLine());
+                j = Int32.Parse(Console.ReadLine());
 
                 Console.WriteLine("Escribe el segundo numero verijon:");
                 b = Int32.Parse(Console.ReadLine());
@@ -85,23 +229,23 @@ namespace BasicCS
                 {
 
                     case "+":
-                        c = a + b;
-                        Console.WriteLine($"La suma de {a} + {b} = {c}");
+                        c = j + b;
+                        Console.WriteLine($"La suma de {j} + {b} = {c}");
                         break;
 
                     case "-":
-                        c = a - b;
-                        Console.WriteLine($"La suma de {a} - {b} = {c}");
+                        c = j - b;
+                        Console.WriteLine($"La suma de {j} - {b} = {c}");
                         break;
 
                     case "*":
-                        c = a * b;
-                        Console.WriteLine($"La suma de {a} * {b} = {c}");
+                        c = j * b;
+                        Console.WriteLine($"La suma de {j} * {b} = {c}");
                         break;
 
                     case "/":
-                        c = a / b;
-                        Console.WriteLine($"La suma de {a} / {b} = {c}");
+                        c = j / b;
+                        Console.WriteLine($"La suma de {j} / {b} = {c}");
                         break;
 
                     default:
